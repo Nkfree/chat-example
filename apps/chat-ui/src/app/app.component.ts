f7b4f5691d990609331484d7931adcdca949b162
Vaclav Svarc <blackvaclav@gmail.com>
2024-06-30 17:32:58 +0200

    feat: added base routing for homepage and chatpage components

--- apps/chat-ui/src/app/app.component.ts
+++ apps/chat-ui/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { exampleFunction } from '@ukol-01/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, RouterOutlet, RouterLink, RouterLinkActive],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- apps/chat-ui/src/app/app.routes.ts
+++ apps/chat-ui/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'home',
+        loadComponent: () => import('../pages/home/homepage.component').then((x) => x.HomepageComponent),
+    },
+    {
+        path: 'chat',
+        loadComponent: () => import('../pages/chat/chatpage.component').then((x) => x.ChatpageComponent),
+    }
+];
