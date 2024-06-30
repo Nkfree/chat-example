6d8c3bd3d4cdeabbc52faf1aac283869cb740962
Vaclav Svarc <blackvaclav@gmail.com>
2024-06-30 17:14:10 +0200

    feat: added homepage and chatpage components to chat-ui

---
+++ apps/chat-ui/src/pages/chat/chatpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chatpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chatpage.component.html',
+  styleUrl: './chatpage.component.css',
+})
+export class ChatpageComponent {}
